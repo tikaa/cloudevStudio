d7e6483afc6be397ed8092efea150a8ce614305a
susinda <susinda@wso2.com>
2014-07-30 15:21:04 +0530

    Adding other files

--- dist/demo/flowchart/demo.js
+++ dist/demo/flowchart/demo.js
@@ -7,6 +7,11 @@ jsPlumb.ready(function() {
 		// case it returns the 'labelText' member that we set on each connection in the 'init' method below.
 		ConnectionOverlays : [
 			[ "Arrow", { location:1 } ],
+			[ "Label", { 
+				location:0.1,
+				id:"label",
+				cssClass:"aLabel"
+			}]
 		],
 		Container:"flowchart-demo"
 	});
@@ -34,10 +39,10 @@ jsPlumb.ready(function() {
 	sourceEndpoint = {
 		endpoint:"Dot",
 		paintStyle:{ 
-			strokeStyle:"transparent",
+			strokeStyle:"#7AB02C",
 			fillStyle:"transparent",
-			radius:5,
-			lineWidth:2 
+			radius:7,
+			lineWidth:3 
 		},				
 		isSource:true,
 		connector:[ "Flowchart", { stub:[40, 60], gap:10, cornerRadius:5, alwaysRespectStubs:true } ],								                
@@ -45,19 +50,28 @@ jsPlumb.ready(function() {
 		hoverPaintStyle:endpointHoverStyle,
 		connectorHoverStyle:connectorHoverStyle,
         dragOptions:{},
-
+        overlays:[
+        	[ "Label", { 
+            	location:[0.5, 1.5], 
+            	label:"Drag",
+            	cssClass:"endpointSourceLabel" 
+            } ]
+        ]
 	},		
 	// the definition of target endpoints (will appear when the user drags a connection) 
 	targetEndpoint = {
 		endpoint:"Dot",					
-		paintStyle:{ fillStyle:"transparent",radius:5 },
+		paintStyle:{ fillStyle:"#7AB02C",radius:11 },
 		hoverPaintStyle:endpointHoverStyle,
 		maxConnections:-1,
 		dropOptions:{ hoverClass:"hover", activeClass:"active" },
 		isTarget:true,			
+        overlays:[
+        	[ "Label", { location:[0.5, -0.5], label:"Drop", cssClass:"endpointTargetLabel" } ]
+        ]
 	},			
 	init = function(connection) {			
-		//connection.getOverlay("label").setLabel(connection.sourceId.substring(15) + "-" + connection.targetId.substring(15));
+		connection.getOverlay("label").setLabel(connection.sourceId.substring(15) + "-" + connection.targetId.substring(15));
 		connection.bind("editCompleted", function(o) {
 			if (typeof console != "undefined")
 				console.log("connection edited. path is now ", o.path);
@@ -78,13 +92,10 @@ jsPlumb.ready(function() {
 	// suspend drawing and initialise.
 	instance.doWhileSuspended(function() {
 
-		//_addEndpoints("Window4", ["TopCenter", "BottomCenter"], ["LeftMiddle", "RightMiddle"]);			
-		//_addEndpoints("Window2", ["LeftMiddle", "BottomCenter"], ["TopCenter", "RightMiddle"]);
-		//_addEndpoints("Window3", ["RightMiddle", "BottomCenter"], ["LeftMiddle", "TopCenter"]);
-		_addEndpoints("Window4", ["RightMiddle"], ["LeftMiddle"]);
-		_addEndpoints("Window2", ["RightMiddle"], ["LeftMiddle"]);
-		_addEndpoints("Window3", ["RightMiddle"], ["LeftMiddle"]);
-		_addEndpoints("Window1", ["RightMiddle"], ["LeftMiddle"]);
+		_addEndpoints("Window4", ["TopCenter", "BottomCenter"], ["LeftMiddle", "RightMiddle"]);			
+		_addEndpoints("Window2", ["LeftMiddle", "BottomCenter"], ["TopCenter", "RightMiddle"]);
+		_addEndpoints("Window3", ["RightMiddle", "BottomCenter"], ["LeftMiddle", "TopCenter"]);
+		_addEndpoints("Window1", ["LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter"]);
 					
 		// listen for new connections; initialise them the same way we initialise the connections at startup.
 		instance.bind("connection", function(connInfo, originalEvent) { 
@@ -98,12 +109,12 @@ jsPlumb.ready(function() {
 		//jsPlumb.draggable(document.querySelectorAll(".window"), { grid: [20, 20] });
         
 		// connect a few up
-		instance.connect({uuids:["Window1RightMiddle", "Window2LeftMiddle"], editable:true});
-		instance.connect({uuids:["Window2RightMiddle", "Window3LeftMiddle"], editable:true});
-		instance.connect({uuids:["Window3RightMiddle", "Window4LeftMiddle"], editable:true});
-		//instance.connect({uuids:["Window3RightMiddle", "Window2RightMiddle"], editable:true});
-		//instance.connect({uuids:["Window4BottomCenter", "Window1TopCenter"], editable:true});
-		//instance.connect({uuids:["Window3BottomCenter", "Window1BottomCenter"], editable:true});
+		instance.connect({uuids:["Window2BottomCenter", "Window3TopCenter"], editable:true});
+		instance.connect({uuids:["Window2LeftMiddle", "Window4LeftMiddle"], editable:true});
+		instance.connect({uuids:["Window4TopCenter", "Window4RightMiddle"], editable:true});
+		instance.connect({uuids:["Window3RightMiddle", "Window2RightMiddle"], editable:true});
+		instance.connect({uuids:["Window4BottomCenter", "Window1TopCenter"], editable:true});
+		instance.connect({uuids:["Window3BottomCenter", "Window1BottomCenter"], editable:true});
 		//
         
 		//
@@ -128,23 +139,5 @@ jsPlumb.ready(function() {
 	});
 
 	jsPlumb.fire("jsPlumbDemoLoaded", instance);
-
 	
 });
-
-
-
-function addElement() {
-
-  var ni = document.getElementById('flowchart-demo');
-
-  var newdiv = document.createElement('div');
-
-  var divIdName = 'flowchartWindow6';
-
-  newdiv.setAttribute('id',divIdName);
-  newdiv.setAttribute('class','window');
-
-
-}
-
